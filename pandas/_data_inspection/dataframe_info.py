4fe39673c10c0df8aaba34bb480ab32f75a5d3a1
rathul85 <rathulhasansaion5@gmail.com>
2026-02-01 00:41:58 +0600

    Update dataframe_info.py

--- pandas/_data_inspection/dataframe_info.py
+++ pandas/_data_inspection/dataframe_info.py
@@ -1,6 +1,7 @@
-from tkinter.font import names
 
-from numpy.ma.extras import column_stack
+#from tkinter.font import names
+
+#from numpy.ma.extras import column_stack
 
 # 1.columns,rows?
 # 2.what type of?
@@ -14,7 +15,7 @@ from numpy.ma.extras import column_stack
 # -memory usage of the data frame
 
 import pandas as pd
-df=pd.read_csv(r"C:\Users\Ratul\Downloads\sales_data_sample (1).csv",encoding="latin1")
+df=pd.read_csv(r"C:\Users\Ratul\Downloads\DimCustomers.csv",encoding="latin1")
 print("Displaying the info of dataset")
 #print(df.to_string())
 print(df.info())
